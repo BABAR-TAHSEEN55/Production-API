936fca224e33a144c55a4fde5d49158e83e8d64f
Babar <babarsmoke24@gmail.com>
2025-05-01 18:18:26 +0530

    feat : setup express app entry point

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import config from "config";
+import logger from "./utils/logger";
+import DatabaseConnection from "./utils/connect";
+import routes from "./routes";
+const port = config.get<number>("port");
+//It takes a generic of type number for port
+const app = express();
+app.listen(port, async () => {
+  logger.info(`Server started sucessfully at ${port}`);
+  // await DatabaseConnection();
+  routes(app);
+});
