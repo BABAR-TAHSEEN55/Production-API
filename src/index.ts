87fb08f07a3a36ef31658a0181217604c637cfb0
Babar <babarsmoke24@gmail.com>
2025-05-05 17:00:32 +0530

    Refactor : Added Helmet for Security

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import config from "config";
 import logger from "./utils/logger";
 import DatabaseConnection from "./utils/connect";
 import routes from "./routes";
+import helmet from "helmet";
 const port = config.get<number>("port");
 //It takes a generic of type number for port
 const app = express();
+app.use(express.json());
+app.use(helmet());
 app.listen(port | 7000, async () => {
   logger.info(`Server started sucessfully at ${port}`);
   await DatabaseConnection();
