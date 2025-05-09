a5d44cd2930a7f77013cc7403006957373e2e35a
Babar <babarsmoke24@gmail.com>
2025-05-09 21:16:57 +0530

    feat : All Routes Handled

--- src/routes.ts
+++ src/routes.ts
@@ -6,17 +6,20 @@ import { CreateUserSessionHanlder } from "./controllers/session.controller";
 import { CreateSessionScheam } from "./schema/session.schema";
 import { getUserSessionsHandler } from "./controllers/session.controller";
 import { RequireUser } from "./middlewares/RequireUser";
+import { UpdateSession } from "./services/session.service.ts";
+import { DeleteSessionHandler } from "./controllers/session.controller.ts";
 function routes(app: Express) {
-  app.get("/healthcheck", (req: Request, res: Response) => {
-    res.sendStatus(200);
-  });
-  app.post("/api/users", Validate(createUserSchema), createUserHandler);
-  app.post(
-    "/api/sessions",
-    Validate(CreateSessionScheam),
-    CreateUserSessionHanlder,
-  );
-  app.get("/api/sessions", RequireUser, getUserSessionsHandler);
+    app.get("/healthcheck", (req: Request, res: Response) => {
+        res.sendStatus(200);
+    });
+    app.post("/api/users", Validate(createUserSchema), createUserHandler);
+    app.post(
+        "/api/sessions",
+        Validate(CreateSessionScheam),
+        CreateUserSessionHanlder,
+    );
+    app.get("/api/sessions", RequireUser, getUserSessionsHandler);
+    app.delete("/api/sessions", RequireUser, DeleteSessionHandler);
 }
 export default routes;
 //TODO : When not provided with email or password req hangs solve this
