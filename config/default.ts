be127b8e520e73019f9ace7ec759a3ea1ef895cc
Babar <babarsmoke24@gmail.com>
2025-05-09 20:41:11 +0530

    chore : Config Implemented

--- config/default.ts
+++ config/default.ts
@@ -3,6 +3,9 @@ dotenv.config();
 export default {
   port: 7000,
   dbUri: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWD}@cluster0.fmjrv.mongodb.net/rest-api`,
-
   SaltWorkFactor: 15,
+  public_key: `${process.env.PUBLIC_KEY}`,
+  private_key: `${process.env.PRIVATE_KEY}`,
+  accessTokenttl: "15m",
+  refreshTokenttl: "1y",
 };
