8a9cd16c73f63246f8cc777186d3f0eb982a8fd9
Babar <babarsmoke24@gmail.com>
2025-05-09 20:40:17 +0530

    feat : Deserialization Middleware Added

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import logger from "./utils/logger";
 import DatabaseConnection from "./utils/connect";
 import routes from "./routes";
 import helmet from "helmet";
+import { DeserializeUser } from "./middlewares/deserializeUser";
 const port = config.get<number>("port");
 //It takes a generic of type number for port
 const app = express();
 app.use(express.json());
 app.use(helmet());
+app.use(DeserializeUser); // This is going to be called on every endPoint
 app.listen(port | 7000, async () => {
   logger.info(`Server started sucessfully at ${port}`);
   await DatabaseConnection();
