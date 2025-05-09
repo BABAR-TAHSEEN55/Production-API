3cbf253a2c917ecdea77229a2858c940dc8a7dc0
Babar <babarsmoke24@gmail.com>
2025-05-09 21:17:25 +0530

    feat : Product Service Implemented

---
+++ src/services/product.service.ts
@@ -0,0 +1,35 @@
+import { FilterQuery } from "mongoose";
+import ProductModel, { ProductDocument } from "../models/product.model.ts";
+import { QueryOptions } from "mongoose";
+
+import { UpdateQuery } from "mongoose";
+
+type ProductInput = Pick<
+    ProductDocument,
+    "title" | "description" | "image" | "price" | "User"
+>;
+export const CreateProduct = async (input: ProductInput) => {
+    try {
+        const product = await ProductModel.create(input);
+        console.log("This is newly Created Prodcut : ", product);
+        return product;
+    } catch (error) {
+        console.log("Error during creation of Prodcut : ", error);
+    }
+};
+export const FindProduct = (
+    query: FilterQuery<ProductDocument>,
+    options: QueryOptions = { lean: true },
+) => {
+    return ProductModel.findOne(query, {}, options); //{}->Rejection
+};
+export const FindProductAndUpdate = (
+    query: FilterQuery<ProductDocument>,
+    update: UpdateQuery<ProductDocument>,
+    options: QueryOptions,
+) => {
+    return ProductModel.findOneAndUpdate(query, update, options);
+};
+export const DeleteProduct = (query: FilterQuery<ProductDocument>) => {
+    return ProductModel.deleteOne(query);
+};
