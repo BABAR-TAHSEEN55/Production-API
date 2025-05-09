c55fb6026800785c9c4d0454ae37c3990e49ca90
Babar <babarsmoke24@gmail.com>
2025-05-09 20:40:38 +0530

    Fix : JWT Keys issue resolved

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,13 +1,12 @@
 import jwt from "jsonwebtoken";
 import config from "config";
-import { object } from "zod";
 const publickey = config.get<string>("public_key");
 const privatekey = config.get<string>("private_key");
 export const SignJwt = (
   object: Object,
   options?: jwt.SignOptions | undefined,
 ) => {
-  return jwt.sign(object, privatekey, {
+  return jwt.sign(object, publickey, {
     ...(options && options),
     // algorithm: "ES256", //TODO : Implement this
   });
@@ -15,6 +14,7 @@ export const SignJwt = (
 export const VerifyJwt = (token: string) => {
   try {
     const decode = jwt.verify(token, publickey);
+    console.log("This is Decode from Utils : ", decode);
     return {
       valid: true,
       expired: false,
@@ -23,8 +23,8 @@ export const VerifyJwt = (token: string) => {
   } catch (error: any) {
     console.log("This is JWT Error");
     return {
-      valid: true,
-      expired: false,
+      valid: false,
+      expired: error.name == "TokenExpiredError",
       decode: null, // Hein ? isn't the scope important?
     };
   }
