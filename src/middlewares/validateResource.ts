9f0b0d2e4acaded048ae7480ec828dc41719be37
Babar <babarsmoke24@gmail.com>
2025-05-31 14:53:27 +0530

    Refactor : Tweaks

--- src/middlewares/validateResource.ts
+++ src/middlewares/validateResource.ts
@@ -1,21 +1,21 @@
 import { AnyZodObject, ZodTypeAny } from "zod";
 import { Request, Response, NextFunction } from "express";
 const Validate =
-  (
-    schema: ZodTypeAny, // This is the issue
-  ) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    try {
-      schema.parse({
-        body: req.body,
-        query: req.query,
-        params: req.params,
-      });
-      next();
-    } catch (error: any) {
-      res.status(400);
-    }
-  };
+    (
+        schema: ZodTypeAny, // This is the issue
+    ) =>
+    (req: Request, res: Response, next: NextFunction) => {
+        try {
+            schema.parse({
+                body: req.body,
+                query: req.query,
+                params: req.params,
+            });
+            next();
+        } catch (error: any) {
+            res.status(400);
+        }
+    };
 
 //This is being used in validationResource in Middlewares and it is actually passed in routes along with Middlewares
 export default Validate;
