308b73ac73796c37c2f37a685ac8497dc89e4b31
Babar <babarsmoke24@gmail.com>
2025-05-31 14:53:09 +0530

    feat : Product schema added

--- src/schema/product.schema.ts
+++ src/schema/product.schema.ts
@@ -1,42 +1,100 @@
-import z, { TypeOf } from "zod";
-export const payload = z.object({
-    body: z.object({
-        title: z.string({
-            required_error: "Title is Required",
+// import z, { TypeOf } from "zod";
+// export const payload = z.object({
+//     body: z.object({
+//         title: z.string({
+//             required_error: "Title is Required",
+//         }),
+//         description: z
+//             .string({
+//                 required_error: "Description is Required",
+//             })
+//             .min(120, "Description should be at least 120 Words"),
+//         image: z.string({
+//             required_error: "Image is Required",
+//         }),
+//         price: z.number({
+//             required_error: "Price is Required",
+//         }),
+//     }),
+// });
+// export const params = z.object({
+//     productId: z.string({
+//         required_error: "Product Id is Required",
+//     }),
+// });
+// export const createProductScheam = z.object({
+//     ...payload.shape, //Gives access to Underlying Schemas
+//     //TODO: Implement this and console it out
+// });
+// export const UpdateProductSchema = z.object({
+//     ...params.shape,
+//     ...payload.shape,
+// });
+// export const DeleteProductSchema = z.object({
+//     ...params.shape,
+// });
+// export const getProductScheam = z.object({
+//     ...params.shape,
+// });
+// export type CreateProductInput = TypeOf<typeof createProductScheam>;
+// export type UpdateProductInput = TypeOf<typeof UpdateProductSchema>;
+// export type DeleteProductInput = TypeOf<typeof DeleteProductSchema>;
+// export type getProductInput = TypeOf<typeof getProductScheam>;
+
+import { number, object, string, TypeOf } from "zod";
+
+// const payload = object({
+//     body : object({
+//
+//     })
+// })
+const payload = {
+    body: object({
+        title: string({
+            required_error: "Title is required ",
         }),
-        description: z
-            .string({
-                required_error: "Description is Required",
-            })
-            .min(120, "Description should be at least 120 Words"),
-        image: z.string({
-            required_error: "Image is Required",
+        description: string({
+            required_error: "Description is required",
+        }).min(80, "Description should at least contain 80 characters"),
+        image: string({
+            required_error: "Image is required",
         }),
-        price: z.number({
-            required_error: "Price is Required",
+        price: number({
+            required_error: "Price is required",
         }),
     }),
-});
-export const params = z.object({
-    productId: z.string({
-        required_error: "Prodcut Id is Required",
+};
+const params = {
+    params: object({
+        ProductId: string({
+            required_error: "Product Id is required",
+        }),
     }),
+};
+
+export const createProductSchema = object({
+    ...payload,
 });
-export const createProductScheam = z.object({
-    ...payload.shape, //Gives access to Underlying Schemas
-    //TODO: Implement this and console it out
+export const updateProductSchema = object({
+    ...payload,
+    ...params,
 });
-export const UpdateProductSchema = z.object({
-    ...params.shape,
-    ...payload.shape,
+
+export const getProductSchema = object({
+    ...params,
 });
-export const DeleteProductSchema = z.object({
-    ...params.shape,
+export const deleteProductSchema = object({
+    ...params,
 });
-export const getProductScheam = z.object({
-    ...params.shape,
-});
-export type createProductInput = TypeOf<typeof createProductScheam>;
-export type UpdateProductInput = TypeOf<typeof UpdateProductSchema>;
-export type DeleteProductInput = TypeOf<typeof DeleteProductSchema>;
-export type getProductInput = TypeOf<typeof getProductScheam>;
+export type createProductInput = TypeOf<typeof createProductSchema>;
+export type UpdateProductInput = TypeOf<typeof updateProductSchema>;
+export type getProductInput = TypeOf<typeof getProductSchema>;
+export type deleteProductInput = TypeOf<typeof deleteProductSchema>;
+//NOTE : If you dont want to wrap {
+//body : object({
+//
+//})
+//}
+//You can directly take ✅ Option 1: Remove the outer body wrapper from Zod schema
+// This is the most common and recommended way when working with Express:
+// and also change you middleware that is parsing it
